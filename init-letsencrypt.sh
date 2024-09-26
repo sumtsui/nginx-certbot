fbe03d051daedef85ba01e87b56e0b2d1e6bf094
Sum Tsui <suxu>
2024-09-26 18:12:34 +0800

    update

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(ai4xm.cn www.ai4xm.cn)
+domains=(cuyuan.cc www.cuyuan.cc)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="sumtsui@gmail.com" # Adding a valid address is strongly recommended
